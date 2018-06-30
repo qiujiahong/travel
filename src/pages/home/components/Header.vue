3ea28ab7b992b96ce5c82d510aa807a7a36ed4e0
qiujiahong <qiujh@szfangle.com>
2018-06-30 08:01:57 +0800

    安装相关css

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      111
+      <div class="header-left"></div>
+      <div class="header-input"></div>
+      <div class="header-right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
