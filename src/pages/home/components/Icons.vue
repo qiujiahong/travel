10b047e2c4d6b910f179afdfd116cc595196dc84
qiujiahong <qiujh@szfangle.com>
2018-06-30 12:10:54 +0800

    完成ICON业务编写；

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,95 +1,111 @@
 <template>
   <div class="icons">
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://pic.616pic.com/ys_b_img/00/03/79/2x1QdC2ZQj.jpg" >
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://pic.616pic.com/ys_b_img/00/03/79/2x1QdC2ZQj.jpg" >
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://pic.616pic.com/ys_b_img/00/03/79/2x1QdC2ZQj.jpg" >
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://pic.616pic.com/ys_b_img/00/03/79/2x1QdC2ZQj.jpg" >
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://pic.616pic.com/ys_b_img/00/03/79/2x1QdC2ZQj.jpg" >
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://pic.616pic.com/ys_b_img/00/03/79/2x1QdC2ZQj.jpg" >
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://pic.616pic.com/ys_b_img/00/03/79/2x1QdC2ZQj.jpg" >
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://pic.616pic.com/ys_b_img/00/03/79/2x1QdC2ZQj.jpg" >
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
+       <swiper>
+         <swiper-slide v-for="(page, index) of pages" :key="index">
+            <div class="icon" v-for="item of page" :key="item.id" >
+              <div class="icon-img">
+                <img class="icon-img-content" :src="item.imgUrl" >
+              </div>
+              <p class="icon-desc">{{item.desc}}</p>
+            </div>
+        </swiper-slide>
+      </swiper>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeIcons'
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://pic.616pic.com/ys_b_img/00/03/79/2x1QdC2ZQj.jpg',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://pic.616pic.com/ys_b_img/00/03/62/z632bbpkr7.jpg',
+        desc: '滑雪季'
+      }, {
+        id: '0003',
+        imgUrl: 'http://pic.616pic.com/ys_b_img/00/03/79/2x1QdC2ZQj.jpg',
+        desc: '滑雪季'
+      }, {
+        id: '0004',
+        imgUrl: 'http://pic.616pic.com/ys_b_img/00/03/79/2x1QdC2ZQj.jpg',
+        desc: '滑雪季4'
+      }, {
+        id: '0005',
+        imgUrl: 'http://pic.616pic.com/ys_b_img/00/03/79/2x1QdC2ZQj.jpg',
+        desc: '滑雪季5'
+      }, {
+        id: '0006',
+        imgUrl: 'http://pic.616pic.com/ys_b_img/00/03/79/2x1QdC2ZQj.jpg',
+        desc: '滑雪季6'
+      }, {
+        id: '0007',
+        imgUrl: 'http://pic.616pic.com/ys_b_img/00/03/79/2x1QdC2ZQj.jpg',
+        desc: '滑雪季7'
+      }, {
+        id: '0008',
+        imgUrl: 'http://pic.616pic.com/ys_b_img/00/03/79/2x1QdC2ZQj.jpg',
+        desc: '滑雪季8'
+      }, {
+        id: '0009',
+        imgUrl: 'http://pic.616pic.com/ys_b_img/00/03/79/2x1QdC2ZQj.jpg',
+        desc: '滑雪季9'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = [ ]
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-  @import '~styles/varibles.styl'
-  .icons
-    width: 100vw
-    overflow: hidden
+  @import '~styles/varibles.styl';
+  @import '~styles/mixins.styl';
+  .icons >>> .swiper-container
     height: 0
     padding-bottom: 50%
-    .icon
-      position: relative
-      overflow: hidden
-      float: left
-      width: 25%
-      height: 0
-      padding-bottom: 25%
-      .icon-img
-        position: absolute
-        top: 0
-        left: 0
-        right: 0
-        bottom: .44rem
-        box-sizing: border-box
-        padding: .1rem
-        .icon-img-content
-          display: block
-          margin: 0 auto
-          height: 100%
-      .icon-desc
-        position: absolute
-        left: 0
-        right: 0
-        bottom: 0
-        height: .44rem
-        line-height: .44rem
-        text-align: center
-        color: $darkTextColor
+  .icon
+    position: relative
+    overflow: hidden
+    float: left
+    width: 25%
+    height: 0
+    padding-bottom: 25%
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: 0.44rem
+      box-sizing: border-box;
+      padding: 0.1rem
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: 0.44rem
+      line-height: 0.44rem
+      text-align: center
+      color: $darkTextColor
+      ellipsis()
 </style>
